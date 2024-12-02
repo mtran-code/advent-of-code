3768f3915b6d50bd0112a0834d60086f79670df3
Michael Tran <mtran256@uwo.ca>
2024-12-02 15:03:50 -0500

    feat: split out performance tests per day

---
+++ 2024/02/test.py
@@ -0,0 +1,21 @@
+import pytest
+from main import part1, part2
+
+input_file = "2024/02/input.txt"
+
+with open(input_file, "r") as f:
+    input_str = [line.strip() for line in f.readlines()]
+
+
+@pytest.mark.benchmark
+def test_1p1():
+    assert part1(input_str) == 680
+
+@pytest.mark.benchmark
+def test_1p2():
+    assert part2(input_str) == 710
+
+@pytest.mark.benchmark
+def test_1():
+    assert part1(input_str) == 680
+    assert part2(input_str) == 710
