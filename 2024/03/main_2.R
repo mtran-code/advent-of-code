ffdb1388807e0f63a6c1a06e1c5cb5ed1450bac4
Michael Tran <mtran256@uwo.ca>
2024-12-03 09:57:05 -0500

    feat[2024/day3]: add alternate solution

---
+++ 2024/03/main_2.R
@@ -0,0 +1,56 @@
+read_input <- function(input_file) {
+  input_file |> readr::read_file()
+}
+
+mul <- function(a, b) {
+  a * b
+}
+
+part1 <- function(input) {
+  input |>
+    stringr::str_extract_all(
+      stringr::regex("mul\\([:digit:]+,[:digit:]+\\)")
+    ) |>
+    unlist() |>
+    tibble::as_tibble_col(column_name = "expr_str") |>
+    dplyr::mutate(expr = purrr::map(expr_str, str2expression)) |>
+    dplyr::mutate(res = unlist(purrr::map(expr, eval))) |>
+    dplyr::pull(res) |>
+    sum() |>
+    print()
+}
+
+part2 <- function(input) {
+  multiplier <- 1
+  do <- function() {
+    multiplier <<- 1
+    0
+  }
+  dont <- function() {
+    multiplier <<- 0
+    0
+  }
+  eval_dos <- function(expr) {
+    eval(expr) * multiplier
+  }
+
+  input |>
+    stringr::str_extract_all(
+      stringr::regex("(mul\\([:digit:]+,[:digit:]+\\))|do\\(\\)|don't\\(\\)")
+    ) |>
+    unlist() |>
+    tibble::as_tibble_col(column_name = "expr_str") |>
+    dplyr::mutate(
+      expr_str = purrr::modify_if(expr_str, \(x) x == "don't()", ~"dont()"),
+      expr = purrr::map(expr_str, str2expression),
+      res = unlist(purrr::map(expr, eval_dos))
+    ) |>
+    dplyr::pull(res) |>
+    sum() |>
+    print()
+}
+
+input_file <- "2024/03/input.txt"
+input <- read_input(input_file)
+part1(input)
+part2(input)
