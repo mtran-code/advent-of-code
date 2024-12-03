8ab0e9b11f497e4543c94cf83a1e71229d9044be
Michael Tran <mtran256@uwo.ca>
2024-12-03 11:24:54 -0500

    fix:[2024/day3]: quick fix to rpy2 package installation

--- 2024/03/test_R.py
+++ 2024/03/test_R.py
@@ -1,16 +1,23 @@
 import pytest
-import rpy2
 import rpy2.robjects as robjects
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector
 
-print(rpy2.__version__)
-print(rpy2.__path__)
+packnames = ("tidyverse")
+
+utils = rpackages.importr("utils")
+utils.chooseCRANmirror(ind=12)
+names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
+if len(names_to_install) > 0:
+    utils.install_packages(StrVector(names_to_install))
 
 
-robjects.r['source']("2024/03/main.R")
+robjects.r["source"]("2024/03/main.R")
 
-part1 = robjects.r['part1']
-part2 = robjects.r['part2']
-input = robjects.r['input']
+part1 = robjects.r["part1"]
+part2 = robjects.r["part2"]
+input = robjects.r["input"]
+
 
 @pytest.mark.benchmark
 def test_3p1_R():
