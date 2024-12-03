af94b408f2ae6065cdd700d70c03815ccdb90c8a
Michael Tran <mtran256@uwo.ca>
2024-12-03 11:42:31 -0500

    fix[2024/day3]: try add R installation to action workflow

--- 2024/03/test_R.py
+++ 2024/03/test_R.py
@@ -1,16 +1,5 @@
 import pytest
 import rpy2.robjects as robjects
-import rpy2.robjects.packages as rpackages
-from rpy2.robjects.vectors import StrVector
-
-packnames = ("tidyverse")
-
-utils = rpackages.importr("utils")
-utils.chooseCRANmirror(ind=12)
-names_to_install = [x for x in packnames if not rpackages.isinstalled(x)]
-if len(names_to_install) > 0:
-    utils.install_packages(StrVector(names_to_install))
-
 
 robjects.r["source"]("2024/03/main.R")
 
