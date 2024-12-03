e0d307f9454becd1034db7a13a60d47f981b8fc4
Michael Tran <mtran256@uwo.ca>
2024-12-03 10:33:02 -0500

    feat[2024/day3]: add performance testing

---
+++ 2024/03/test.py
@@ -0,0 +1,23 @@
+import pytest
+from main import part1, part2
+
+input_file = "2024/03/input.txt"
+
+with open(input_file, "r") as f:
+    input_str = "".join([line.strip() for line in f.readlines()])
+
+
+@pytest.mark.benchmark
+def test_2p1():
+    assert part1(input_str) == 159833790
+
+
+@pytest.mark.benchmark
+def test_2p2():
+    assert part2(input_str) == 89349241
+
+
+@pytest.mark.benchmark
+def test_2():
+    assert part1(input_str) == 159833790
+    assert part2(input_str) == 89349241
